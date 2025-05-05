de28a56cce8d6d4ba58d541ce85546fb44e2366e
verazi <ziyuw12@student.unimelb.edu.au>
2025-05-05 18:51:18 +1000

    Delete bank_system/message_factory.py

--- bank_system/message_factory.py
+++
@@ -1,8 +0,0 @@
-from .message import Message
-
-class MessageFactory:
-    """A factory for de-serialising messages."""
-
-    @staticmethod
-    def deserialise(data: str) -> Message:
-        pass
